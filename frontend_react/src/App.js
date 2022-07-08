a6e4fda30b84c6826964864118af894b821c5b4c
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-08 12:31:34 -0700

    update app.scss with styling

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
+import './App.scss';
 
 const App = () => {
   return (
