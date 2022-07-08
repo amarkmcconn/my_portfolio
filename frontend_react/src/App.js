85768fd35ab96d1b9c5228cea0fa55aa062f7be9
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-08 11:39:50 -0700

    update file structure for navbar component

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
 
 const App = () => {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Work />
