501a7fa3ff5547a86b5b3cc0b6d030f6f1ef9a54
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-08 18:43:32 -0700

    update styling and images

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -5,20 +5,19 @@ import cpp from '../assets/cpp.png';
 import csharp from '../assets/csharp.svg'
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
+
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import mu5 from '../assets/mu5.png';
 import node from '../assets/node.png';
-import python from '../assets/python.png';
+
 import react from '../assets/react.png';
 import react1 from '../assets/react1.svg';
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
-import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
+
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
@@ -35,8 +34,7 @@ import amazon from '../assets/amazon.png';
 import asus from '../assets/asus.png';
 import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
+
 
 export default {
   email,
@@ -46,20 +44,16 @@ export default {
   css,
   csharp,
   figma,
-  flutter,
   git,
   graphql,
   html,
   javascript,
   mu5,
   node,
-  python,
   react,
   react1,
   redux,
   sass,
-  typescript,
-  vue,
   about01,
   about02,
   about03,
@@ -73,6 +67,4 @@ export default {
   asus,
   bolt,
   nb,
-  skype,
-  spotify,
 };
