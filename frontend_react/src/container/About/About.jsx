e95551be28a814c78ecc2bfae3b1dde916e1133a
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-15 12:36:03 -0700

    update styling/format

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -15,7 +15,12 @@ const About = () => {
   return (
     <>
       <h2 className="head-text"> About <span>Me</span></h2>
-      <p></p>
+      <p className='p-text1'>I'm a current student at Epicodus. I bring enthusiasm and understanding of various programming languages to webpage planning and development. I have experience designing and developing sites from concept to roll out. I have a foundational grasp of various CSS and HTML options and development platforms. <br /> 
+      <br />
+      I've worked in both customer-facing and behind-the-scenes roles throughout my professional work experience. My professional work experience includes call centers, Insurance, and the construction industry. I'm highly adept at cultivating teams to have a positive influence on company objectives. Once teams have been formed, I enjoy being a motivated, results-oriented leader. I push to understand my team's wants and needs. <br /> 
+      <br />
+      I'm also capable of working on my own with little to no direction. I'm comprehensive and detailed, and I like to understand the why behind objectives. I'm excited to continue learning about different programming and test/apply my knowledge to real-world projects.
+      </p>
       <div className="app__profiles" id="about">
         {abouts.map((about, index) => (
           <motion.div
