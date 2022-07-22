ace6fbeaa59d8878c4af45612e3a232acf311e7e
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-22 13:19:33 -0700

    BUG resolved

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -46,30 +46,42 @@ const Skills = () => {
           ))}
         </motion.div>
         <motion.div className='app__skills-exp'>
-            {console.log()}
-            {experience?.works?.map((work) => (
-              <>
-                <motion.div
-                  whileInView={{ opacity: [0,1]}}
-                  transition={{ duration: 0.5 }}
-                  className='app__skills-exp-work'
-                  data-tip
-                  data-work={work.name}
-                  key={work.name}
-                >
-                  <h4 className='bold-text'>{work.name}</h4>
-                  <p className='p-text'>{work.company}</p>
-                  <ReactTooltip
-                    ID={work.name}
-                    effect="solid"
-                    arrowColor='#fff'
-                    className='skills-tooltip'
-                  >
-                    {work.desc}
-                  </ReactTooltip>
+            {experience?.map((experience) => (
+              <motion.div
+                className='app__skills-exp-item'
+                key={experience.year}
+              >
+                <div className='app__skills-exp-year'>
+                  <p className='bold-text'>{experience.year}</p>
+                </div>
+                <motion.div className='app__skills-exp-works'>
+                  {experience.works.map((work) => (
+                    <>
+                      <motion.div
+                        whileInView={{ opacity: [0,1]}}
+                        transition={{ duration: 0.5 }}
+                        className='app__skills-exp-work'
+                        data-tip
+                        data-work={work.name}
+                        key={work.name}
+                      >
+                        <h4 className='bold-text'>{work.name}</h4>
+                        <p className='p-text'>{work.company}</p>
+                        <ReactTooltip
+                          ID={work.name}
+                          effect="solid"
+                          arrowColor='#fff'
+                          className='skills-tooltip'
+                        >
+                          {work.desc}
+                        </ReactTooltip>
+                      </motion.div>
+                    </>
+                  ))}
                 </motion.div>
-              </>
+              </motion.div>
             ))}
+         
         </motion.div>
       </div>    
     </>
