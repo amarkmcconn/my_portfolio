e1eace67fc7001122e685b78becf3c5302e97671
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-26 15:46:35 -0700

    update styling

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -19,20 +19,20 @@ const Header = () => {
     <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0,1]}}
-        transition={{duration: 1}}
+        transition={{duration: 2}}
         className="app__header-info"
       >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
-            <span>🖐</span>
+            <span>👋🏻</span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello, I am</p>
               <h1 className="head-text">Mark McConnell</h1>
             </div>
           </div>
           <div className="tag-cmp app__flex">
-            <p className="p-text">Full Stack Web Developer</p>
-            <p className="p-text">Currently seeking opportunities</p>
+            <p className="p-text">Full Stack Developer</p>
+            <p className="p-text">Open to opportunities</p>
           </div>
         </div>
       </motion.div>
@@ -40,7 +40,7 @@ const Header = () => {
       <div className="app__header_profile-img">
       <motion.div 
         whileInView={{ opacity: [0,1]}}
-        transition={{duration: 1, delayChildren: 1}}
+        transition={{duration: 2, delayChildren: 1}}
         className="app__header-img"
       >
        <img src={images.profile} alt="profile_bg" />
