8dae94de68b802459086c10a6723ef75a6da2fab
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-27 15:21:56 -0700

    update navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <img src={images.logo} alt="logo"/>
       </div>
       <ul className="app__navbar-links">
-        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+        {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
